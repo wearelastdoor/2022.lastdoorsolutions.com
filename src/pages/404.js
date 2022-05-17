cd51b2d5b563f6815faa840524dd2940a4fe6510
Bishal Mishra <bishaller@gmail.com>
2022-05-17 22:25:38 +0545

    fix: 404 page issue

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,58 +4,61 @@ import TransitionLink from "gatsby-plugin-transition-link"
 import {newContent, animateObjects} from "../components/layout"
 import Footer from "../components/footer";
 
+const browser = typeof window !== "undefined" && window;
 const NotFoundPage = () => {
     return (
-        <>
-            <main className="main">
-                <Seo/>
-                <section className="c-hero c-hero--gutter-bottom">
-                    <div className="l-container">
-                        <h1 className="c-hero__title" title={`404: Not Found`}>
-                            404: Not Found
-                        </h1>
-                        <div className="c-hero__description">
-                            <p>
-                                The page you’re looking for can’t be found. The page may have been
-                                deleted or never existed. If you want some information regarding
-                                anything related to Last Door,{" "}
-                                <TransitionLink
-                                    to="/contact/"
+        browser && (
+            <>
+                <main className="main">
+                    <Seo/>
+                    <section className="c-hero c-hero--gutter-bottom">
+                        <div className="l-container">
+                            <h1 className="c-hero__title" title={`404: Not Found`}>
+                                404: Not Found
+                            </h1>
+                            <div className="c-hero__description">
+                                <p>
+                                    The page you’re looking for can’t be found. The page may have been
+                                    deleted or never existed. If you want some information regarding
+                                    anything related to Last Door,{" "}
+                                    <TransitionLink
+                                        to="/contact/"
 
-                                    exit={{
-                                        length: 0.6,
-                                        trigger: ({exit, e, node}) => animateObjects(exit, node),
-                                    }}
-                                    entry={{
-                                        delay: 0.7,
-                                        trigger: ({entry, node}) => newContent(node),
-                                    }}
-                                >
-                                    contact us
-                                </TransitionLink>
-                                .
-                            </p>
+                                        exit={{
+                                            length: 0.6,
+                                            trigger: ({exit, e, node}) => animateObjects(exit, node),
+                                        }}
+                                        entry={{
+                                            delay: 0.7,
+                                            trigger: ({entry, node}) => newContent(node),
+                                        }}
+                                    >
+                                        contact us
+                                    </TransitionLink>
+                                    .
+                                </p>
+                            </div>
+                            <TransitionLink
+                                to="/"
+                                className="c-btn c-btn--primary"
+                                exit={{
+                                    length: 0.6,
+                                    trigger: ({exit, e, node}) => animateObjects(exit, node),
+                                }}
+                                entry={{
+                                    delay: 0.7,
+                                    trigger: ({entry, node}) => newContent(node),
+                                }}
+                            >
+                                Back to homepage
+                                <span></span>
+                            </TransitionLink>
                         </div>
-                        <TransitionLink
-                            to="/"
-                            className="c-btn c-btn--primary"
-                            exit={{
-                                length: 0.6,
-                                trigger: ({exit, e, node}) => animateObjects(exit, node),
-                            }}
-                            entry={{
-                                delay: 0.7,
-                                trigger: ({entry, node}) => newContent(node),
-                            }}
-                        >
-                            Back to homepage
-                            <span></span>
-                        </TransitionLink>
-                    </div>
-                </section>
-            </main>
-            <Footer/>
-        </>
+                    </section>
+                </main>
+                <Footer/>
+            </>
+        )
     )
 }
 

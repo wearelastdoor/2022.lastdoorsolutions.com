976d9611f7acb4d6dca5b7b3aafd581a1616973d
sunitanep <sunita@lastdoorsolutions.com>
2024-01-08 01:30:54 +0545

    Added past clients carousel

--- src/components/pageBanner.js
+++ src/components/pageBanner.js
@@ -1,31 +1,38 @@
-import * as React from "react"
+import * as React from "react";
 
-const PageBanner = ({ title, pageName, themeType, containerWidth, pageDescription }) => {
-
+const PageBanner = ({
+  title,
+  pageName,
+  themeType,
+  containerWidth,
+  pageDescription,
+}) => {
   return (
-    <section
-      className={`c-page-banner ${
-        themeType ? themeType : "light"
-      }`}
-    >
+    <section className={`c-page-banner ${themeType ? themeType : "light"}`}>
       <div className="l-container">
         <div className="c-page-banner__content">
           <p className="c-page-banner__page-name">{pageName}</p>
           <h1
             className="c-page-banner__title"
             title={title}
-            style={{ maxWidth: containerWidth ? containerWidth : 810 }}
+            style={{
+              maxWidth: containerWidth ? `${containerWidth}px` : `${810}px`,
+            }}
           >
             {title}
           </h1>
-          <div className={`c-hero-intro__description`}>
-              <p>{pageDescription}</p>
+          <div
+            className={`c-hero-intro__description`}
+            style={{
+              maxWidth: containerWidth ? `${containerWidth}px` : `100%`,
+            }}
+          >
+            <p>{pageDescription}</p>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default PageBanner
+export default PageBanner;
---
+++ src/components/work/PastClients.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import Slider from "react-slick";
+
+const PastClients = () => {
+  const data = useStaticQuery(graphql`
+    {
+      clients: pastClientsJson {
+        logos {
+          image {
+            baseUrl {
+              childImageSharp {
+                fluid(quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            alt
+            width
+          }
+        }
+      }
+    }
+  `);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  };
+
+  return (
+    <Slider {...settings} className="past-clients">
+      {data.clients.logos.map(({ image }, i) => {
+        const { baseUrl, alt, width } = image;
+        return (
+          <div style={{ width: `${width}px` }}>
+            <Img
+              fluid={baseUrl.childImageSharp.fluid}
+              alt={alt}
+              style={{ maxWidth: `${width}px` }}
+            />
+          </div>
+        );
+      })}
+    </Slider>
+  );
+};
+
+export default PastClients;
